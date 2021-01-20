bc0b6537fb7e4dd20292adc7c9884663bd9bf012
rmoak <rcmoak@gmail.com>
2021-01-20 12:30:35 -0500

    First commit.

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.utility.DockerImageName;
+
+import static org.testcontainers.containers.localstack.LocalStackContainer.Service.DYNAMODB;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+
+        LocalStackContainer localstack =
+                new LocalStackContainer(DockerImageName.parse("localstack/localstack:latest")
+                        .asCompatibleSubstituteFor("localstack/localstack"));
+        localstack.withServices(DYNAMODB).start();
+     /*   AmazonDynamoDB db =
+                AmazonDynamoDBClientBuilder.standard()
+                        .withEndpointConfiguration(localstack.getEndpointConfiguration(DYNAMODB))
+                        .withCredentials(localstack.getDefaultCredentialsProvider())
+                        .build();*/
+
+    }
+}
